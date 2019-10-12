be224e8514b6ecce2d49858d4cc9d5cdf2174c57
Michael Waldo <mbwcogic@yahoo.com>
2019-10-12 23:59:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
  elsif phrase == phrase.upcase
      return "NO, NOT SINCE 1938!"
  elsif phrase != phrase.upcase
-    return "HUH?! SPEAK UP,SONNY!" 
+    return "HUH?! SPEAK UP, SONNY!" 
   end
 end
    
